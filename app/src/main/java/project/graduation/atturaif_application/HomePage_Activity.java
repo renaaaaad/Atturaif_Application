7f21c1c75f3e7dfcb494784a6c85c9cec2a80ba2
nora <noraalnjeem@gmail.com>
2019-08-20 14:50:51 +0300

    home page
    home page

---
+++ app/src/main/java/project/graduation/atturaif_application/HomePage_Activity.java
@@ -0,0 +1,14 @@
+package project.graduation.atturaif_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomePage_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page_);
+    }
+}
