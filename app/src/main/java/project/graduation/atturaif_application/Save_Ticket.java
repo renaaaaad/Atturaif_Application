3d02632f3a88b6af2843457bce53a67e57cb940e
Fadiasalami <47189814+Fadiasalami@users.noreply.github.com>
2019-11-05 12:11:08 +0300

    tt

--- app/src/main/java/project/graduation/atturaif_application/Save_Ticket.java
+++ app/src/main/java/project/graduation/atturaif_application/Save_Ticket.java
@@ -23,7 +23,7 @@ import java.io.IOException;
 
 public class Save_Ticket extends AppCompatActivity {
     Toolbar toolbar;
-    TextView price_total, type, date;
+    TextView price_total, date;
     String[] descriptionData = {"Book Ticket", "View Ticket", "Payment","Save Ticket"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +32,10 @@ public class Save_Ticket extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        type = findViewById(R.id.type);
         date = findViewById(R.id.date);
         price_total = findViewById(R.id.price_total);
         float price = MySharedPreference.getFolat(getApplicationContext(), Constant.Keys.User_PRICE, 0);
         price_total.setText(Float.toString(price) + " " + getApplicationContext().getString(R.string.s_r));
-        type.setText(MySharedPreference.getString(getApplicationContext(), Constant.Keys.TOUT_TYPE, "Guided"));
         date.setText(MySharedPreference.getString(getApplicationContext(), Constant.Keys.BOOKING_DATE, ""));
         MySharedPreference.putFloat(getApplicationContext(), Constant.Keys.User_PRICE, 0);
 
@@ -51,14 +49,14 @@ public class Save_Ticket extends AppCompatActivity {
         state = Environment.getExternalStorageState();
 
         if (Environment.MEDIA_MOUNTED.equals(state)) {
-            File Root = getExternalFilesDir("Atturaif folder");
+            File Root = Environment.getExternalStorageDirectory();
             File Dir = new File(Root.getAbsolutePath() + "/Downloads");
             if (!Dir.exists()) {
 
                 Dir.mkdir();
             }
             File file = new File(Dir, "Ticket Information.txt");
-            String Message ="Type:"+ type.getText().toString()+"\n"+"Date:"+ date.getText().toString()+"\n"+"Price:"+ price_total.getText().toString();
+            String Message ="Date:"+ date.getText().toString()+"\n"+"Price:"+ price_total.getText().toString();
 
 
 
@@ -67,14 +65,8 @@ public class Save_Ticket extends AppCompatActivity {
             try {
                 FileOutputStream fileOutputStream = new FileOutputStream(file);
                 fileOutputStream.write(Message.getBytes());
-
-
                 fileOutputStream.close();
-
-
-
-
-                Toast.makeText(getApplicationContext(), "Ticket saved in "+Root.getPath(), Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Ticket saved in Storage/Downloads", Toast.LENGTH_LONG).show();
 
 
             } catch (FileNotFoundException e) {
